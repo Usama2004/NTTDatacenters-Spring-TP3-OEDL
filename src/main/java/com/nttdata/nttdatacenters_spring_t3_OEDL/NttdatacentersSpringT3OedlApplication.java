c5fd53faaf8cf29f67f76b5945cf99a24040cfa7
oeddahab@emeal.nttdata.com <oeddahab@emeal.nttdata.com>
2024-10-30 15:54:13 +0100

    Este es el primer commit, todavia no hay acualizaciones de codigo

---
+++ src/main/java/com/nttdata/nttdatacenters_spring_t3_OEDL/NttdatacentersSpringT3OedlApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.nttdatacenters_spring_t3_OEDL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NttdatacentersSpringT3OedlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NttdatacentersSpringT3OedlApplication.class, args);
+	}
+
+}
